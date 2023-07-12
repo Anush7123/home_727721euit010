2add8b2d0b320b1acddc9c832e61fa4ef7913d5d
Anush7123 <94896276+Anush7123@users.noreply.github.com>
2023-07-12 09:56:41 +0530

    Add files via upload

---
+++ src/home.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './home.css';
+
+function logins() {
+  return (
+    <div>
+  <header class="header">
+    <a href="#" class="logo">SafeWheels</a>
+    <nav class="nav-items">
+      <a href="#">Login</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a> 
+    </nav>
+  </header>
+  <main>
+    <div class="intro">
+      <h1>Automobile Insurance</h1>
+      <p></p>
+      <button>Register</button>
+    </div>
+    
+        
+    
+    
+    <div class="about-me">
+      
+      
+    </div>
+    </main>
+  
+  </div>
+)
+}
+
+export default logins
